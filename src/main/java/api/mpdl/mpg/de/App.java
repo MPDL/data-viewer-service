d88a5006ab475c1a759a3e9905ed3472a736bfb2
Vladislav Makarenko <makarenko@mpdl.mpg.de>
2014-09-30 19:14:57 +0200

    Initial commit

---
+++ src/main/java/api/mpdl/mpg/de/App.java
@@ -0,0 +1,13 @@
+package api.mpdl.mpg.de;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
